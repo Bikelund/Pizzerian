003331a074625dfad09d16868a253306ae1619f3
Tengil91 <kachiun91@gmail.com>
2019-01-24 14:17:48 +0100

    om någon har laddat upp igen så vet jag inte vad jag kommer göra x_x

--- price.js
+++ price.js
@@ -1,5 +1,6 @@
 let pizzas = document.querySelectorAll('.pizzas p');
 let order = document.getElementById('order');
+let price = document.getElementById('price');
 let list = [];
 let cost = 0;
 for(let i=0;i<pizzas.length;i++){
@@ -15,6 +16,8 @@ function updateOrder(){
     order.innerHTML = "";
     for (let i=0;i<list.length;i++){
         order.innerHTML += "<li>" + list[i] + "</li>";
+        console.log(order);
     }
     order.innerHTML += "<li>Total kostnad: " + cost + " kr</li>"
+    order.style.display = 'block';
 }
